2a19893d08adb4b34a70ce6ab0ca9107c2cc3fb8
Mateusz Kubisiak <matesz1kb@gmail.com>
2023-03-21 18:16:09 +0100

    Added `viewSlice`

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import "@/styles/global.scss";
+import "normalize.css";
 import { Layout } from "@/components/Layout/Layout";
-import { Routes, RouteFactory } from "./routes";
+import { Routes, RouteFactory } from "@/routes";
+import { store } from "@/redux/store";
+import { Provider } from "react-redux";
 
 function App() {
 	return (
 		<Layout>
-			<RouteFactory routes={Routes} />
+			<Provider store={store}>
+				<RouteFactory routes={Routes} />
+			</Provider>
 		</Layout>
 	);
 }
---
+++ src/redux/store/viewSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+
+export type View = "list" | "tiles";
+
+export interface ViewState {
+	view: View;
+}
+
+const initialState: ViewState = {
+	view: "list",
+};
+
+const viewSlice = createSlice({
+	name: "search",
+	initialState,
+	reducers: {
+		setView: (state, action: PayloadAction<View>) => {
+			state.view = action.payload;
+		},
+	},
+});
+
+export const { setView } = viewSlice.actions;
+export default viewSlice.reducer;
