e40617afcde5a9dc00c999ffb033497e1e48c45d
Mateusz Kubisiak <matesz1kb@gmail.com>
2023-03-25 16:35:39 +0100

    Extended `Home` page to use query

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,46 @@
+import { useLayoutEffect, useMemo } from "react";
+import {
+	resetCountry,
+	setQuery,
+	setAmount,
+	setNews,
+} from "@/redux/store/newsSlice";
+
+import { topics } from "@/constants";
+import { getRandomElementFromArray } from "@/utilities";
+import { useAppDispatch, useNews } from "@/hooks";
+
 export const Home = () => {
+	const { newsByQuery } = useNews();
+
+	const searchQuery = useMemo(
+		() =>
+			getRandomElementFromArray({
+				type: "string",
+				array: topics,
+			}),
+		[]
+	);
+	const { status, data, error } = newsByQuery("bitcoin");
+
+	const dispatch = useAppDispatch();
+
+	useLayoutEffect(() => {
+		if (data) {
+			dispatch(setQuery(searchQuery));
+			dispatch(setNews(data.articles));
+			dispatch(setAmount(data.totalResults));
+			dispatch(resetCountry);
+		}
+	}, [data]);
+
+	if (status === "loading") {
+		// TODO: Implement Loader
+		return <div>Loading...</div>;
+	}
+	if (status === "error") {
+		return <div>Error: {error.message}</div>;
+	}
+
 	return <div>Home</div>;
 };
