c6f84735b244ce44bb6d6942da88ebea46246138
Mateusz Kubisiak <matesz1kb@gmail.com>
2023-03-25 16:28:43 +0100

    Added article counter in `Footer`

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -1,19 +1,21 @@
+import { useAppSelector } from "@/hooks";
 import { FC } from "react";
 import { Clock } from "./Clock";
 import s from "./Footer.module.scss";
 
+
 export const Footer: FC = () => {
+	const articlesAmount = useAppSelector((state) => state.news.amount);
+
 	const year = new Date().getFullYear();
 
-	// TODO: Add dynamic article counter
-	const articles = 15;
 	return (
 		<footer className={s.footerContainer}>
 			<div className={`${s.footerWrapper} container`}>
 				<div className={s.copyWrapper}>
 					<p className={s.copy}>ngNews © {year}</p>
 					<p className={s.amountLabel}>Articles on page</p>
-					<p className={s.amountWrapper}>{articles}</p>
+					<p className={s.amountWrapper}>{articlesAmount}</p>
 				</div>
 				<Clock />
 			</div>
