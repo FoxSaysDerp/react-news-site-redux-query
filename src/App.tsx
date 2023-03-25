833106a7cd8d00ed1504cba45e31ebf5ee3a1288
Mateusz Kubisiak <matesz1kb@gmail.com>
2023-03-25 16:20:13 +0100

    Added `QueryClient`

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import "@/styles/global.scss";
 import { Layout } from "@/components/Layout/Layout";
 import { Routes, RouteFactory } from "@/routes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 
 function App() {
+	const queryClient = new QueryClient();
+
 	return (
-		<Layout>
-			<RouteFactory routes={Routes} />
-		</Layout>
+		<QueryClientProvider client={queryClient}>
+			<Provider store={store}>
+				<Layout>
+					<RouteFactory routes={Routes} />
+				</Layout>
+			</Provider>
+		</QueryClientProvider>
 	);
 }
 
